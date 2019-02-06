54032e346a644e9324a6bddc561b932f44821041
Hiiragi Kugiha <46911299+kugiha@users.noreply.github.com>
2019-02-07 03:28:32 +0900

    Remove extra space

--- slack-vrc/src/monitor.py
+++ slack-vrc/src/monitor.py
@@ -46,7 +46,7 @@ class UserMonitor:
         if (self.monitor_type == UserMonitorType.WORLD_MOVE) and (old.worldId != new.worldId):
             old_worldname = a.getWorldById(old.worldId).name if old.worldId is not None else ':lock:'
             new_worldname = a.getWorldById(new.worldId).name if new.worldId is not None else ':lock:'
-            self.message.reply('{}\n>: {} -> `{}`'.format(str(self), old_worldname, new_worldname))
+            self.message.reply('{}\n>{} -> `{}`'.format(str(self), old_worldname, new_worldname))
     def __str__(self):
         return '{} for {}'.format(self.user.displayName, self.monitor_type)
 class UserMonitorType(Enum):
