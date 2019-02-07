8220166a028f992bb93a90374d3c1c91cfb748c1
Hiiragi Kugiha <46911299+kugiha@users.noreply.github.com>
2019-02-07 13:47:58 +0900

    Fix get_friend_by_display_name

--- slack-vrc/src/plugins/monitor_com.py
+++ slack-vrc/src/plugins/monitor_com.py
@@ -12,17 +12,19 @@ def monitor_user(message, mode, displayName):
     if monitor_type is None:
         message.reply('No such mode')
         return
-    user = get_friend_id_by_display_name(displayName)
+    user = get_friend_by_display_name(displayName)
     if user is None:
         message.reply('User not found')
         return
     user_monitor = UserMonitor(user, monitor_type, message)
 
-def get_friend_id_by_display_name(displayName):
-    friends = a.getFriends(offline=False)
-    friends.extend(a.getFriends(offline=True))
-    for f in friends:
+def get_friend_by_display_name(displayName):
+    for f in get_all_friends():
         print(f.displayName)
         if displayName == f.displayName:
             return f
     return None
+def get_all_friends():
+    friends = a.getFriends(offline=False)
+    friends.extend(a.getFriends(offline=True))
+    return friends
