c199af82528feb782eb9989e67aa982eb7bff2ed
Hiiragi Kugiha <46911299+kugiha@users.noreply.github.com>
2019-02-21 00:41:48 +0900

    Fix command text parsing

--- slack-vrc/src/main.py
+++ slack-vrc/src/main.py
@@ -3,6 +3,6 @@ from utils import respond
 from vrconline import online
 def run_command(user, command, channel, command_text):
     command_text_list = command_text.split()
-    if command_text[0] in ['online', 'o']:
+    if command_text_list[0] in ['online', 'o']:
         respond(None, online())
     return respond(None, "%s invoked %s in %s with the following text: %s" % (user, command, channel, command_text))
