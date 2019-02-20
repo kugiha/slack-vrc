d4b26c94fd1a295c6ef6f1fb2543bc1c7cad723f
Hiiragi Kugiha <46911299+kugiha@users.noreply.github.com>
2019-02-21 00:23:08 +0900

    Fix import

--- slack-vrc/src/main.py
+++ slack-vrc/src/main.py
@@ -1,8 +1,8 @@
 import json
 from utils import respond
-import commands
+import commands.vrconline import online
 def run_command(user, command, channel, command_text):
     command_text_list = command_text.split()
     if command_text[0] in ['online', 'o']:
-        respond(commands.online())
+        respond(online())
     return respond(None, "%s invoked %s in %s with the following text: %s" % (user, command, channel, command_text))
