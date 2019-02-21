b0d8535391807c07a86dc08c668287c85419d38f
Hiiragi Kugiha <46911299+kugiha@users.noreply.github.com>
2019-02-22 02:35:48 +0900

    Fix fetch.py

--- slack-vrc/fsf/fetch.py
+++ slack-vrc/fsf/fetch.py
@@ -2,7 +2,7 @@ from vrc_auth import vrc
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 table_name = "slackvrc_friend_status"
-def lambda_handler():
+def lambda_handler(_, __):
     friends = vrc.getFriends(offline=False)
     friends.extend(vrc.getFriends(offline=True))
     for f in friends:
@@ -22,7 +22,7 @@ def lambda_handler():
                 row['world_id'] = f.location.worldId
                 row['instance_id'] = f.location.instanceId
                 world = vrc.getWorldById(f.location.worldId)
-                instance = vrc.getInstanceById(f.location.instanceId)
+                instance = vrc.getInstanceById(f.location.worldId, f.location.instanceId)
 def save_to_db(payload):
     dynamodb = boto3.resource('dynamodb')
     dynamotable = dynamodb.Table(table_name)
