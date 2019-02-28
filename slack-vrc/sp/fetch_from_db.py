e53369423446a1f68e0f5d7b680385625d91aedf
Hiiragi Kugiha <46911299+kugiha@users.noreply.github.com>
2019-03-01 02:46:48 +0900

    Fix environ key

--- slack-vrc/sp/fetch_from_db.py
+++ slack-vrc/sp/fetch_from_db.py
@@ -1,7 +1,7 @@
 import os
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
-table_name = os.environ['slackvrc_friend_status']
+table_name = os.environ['friends_table_name']
 dynamodb = boto3.resource('dynamodb')
 dynamotable = dynamodb.Table(table_name)
 
