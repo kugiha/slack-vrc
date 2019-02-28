81c8269ed02b8734e555e5cfc8a57f8862ebccec
Hiiragi Kugiha <46911299+kugiha@users.noreply.github.com>
2019-03-01 01:39:38 +0900

    Remove unused import

--- slack-vrc/fsf/fetch.py
+++ slack-vrc/fsf/fetch.py
@@ -1,6 +1,5 @@
 from vrc_auth import vrc
 import boto3
-from boto3.dynamodb.conditions import Key, Attr
 table_name = "slackvrc_friend_status"
 dynamodb = boto3.resource('dynamodb')
 dynamotable = dynamodb.Table(table_name)
