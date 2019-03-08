6b953ac53ce1aee468f511ca15e1bbfb61412e08
Hiiragi Kugiha <46911299+kugiha@users.noreply.github.com>
2019-03-08 23:12:07 +0900

    Format

--- slack-vrc/sp/vrconline.py
+++ slack-vrc/sp/vrconline.py
@@ -1,6 +1,7 @@
 from utils import get_instanceStatus_emoji
-from fetch_from_db import getOnlineFriends
+from fetch_from_db import getOnlineFriends, getWorldInfoById, getInstanceInfoById
 from datetime import datetime, timezone, timedelta
+
 
 def online():
     friends = getOnlineFriends()
@@ -8,13 +9,16 @@ def online():
     reply = '\nYour online friends (as of {}):'.format(as_of_string)
     for f in friends:
         reply += '\n* '
-        if f.get('instance_type')=='private':
+        if f.get('instance_type') == 'private':
             # private
             reply += '*{}* :lock:'.format(f['name'])
         else:
             # public, friends+, etc...
-            reply += '*{}* _in {}_{}'.format(f['name'], f['world_name'], get_instanceStatus_emoji(f['instance_type']))
+            reply += '*{}* _in {}_{}'.format(
+                f['name'], f['world_name'],
+                get_instanceStatus_emoji(f['instance_type']))
     return reply
+
 
 def online_grouped():
     friends = getOnlineFriends()
@@ -22,7 +26,7 @@ def online_grouped():
     users_by_world = {}
     private_users = []
     for f in friends:
-        if f.get('instance_type')=='private':
+        if f.get('instance_type') == 'private':
             # private
             private_users.append(f)
         else:
@@ -30,7 +34,9 @@ def online_grouped():
             if f['instance_id'] not in users_by_world.keys():
                 users_by_world[f['instance_id']] = []
             users_by_world[f['instance_id']].append(f)
-    users_by_world = sorted(users_by_world.items(), key=lambda x: -len(x[1])) # {'instance_id': [users array]}
+    users_by_world = sorted(
+        users_by_world.items(),
+        key=lambda x: -len(x[1]))  # {'instance_id': [users array]}
     reply = '\nOnline friends (grouped) (as of {})\n'.format(as_of_string)
     for item in users_by_world:
         reply += '* '
@@ -38,12 +44,17 @@ def online_grouped():
         for user in users:
             reply += '*{}*, '.format(user['name'])
         reply = reply[:-2]
-        reply += ' _in {}_ ({}/{}) {}\n'.format(users[0]['world_name'], users[0]['instance_users_count'], users[0]['world_capacity'], get_instanceStatus_emoji(users[0]['instance_type']))
+        reply += ' _in {}_ ({}/{}) {}\n'.format(
+            users[0]['world_name'], users[0]['instance_users_count'],
+            users[0]['world_capacity'],
+            get_instanceStatus_emoji(users[0]['instance_type']))
     reply += 'in-private friends\n'
     for user in private_users:
         reply += '{},'.format(user['name'])
     reply = reply[:-1]
     return reply
 
+
 def get_as_of_string(updated_at):
-    return datetime.fromtimestamp(updated_at, timezone(timedelta(hours=9))).strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.fromtimestamp(updated_at, timezone(
+        timedelta(hours=9))).strftime('%Y-%m-%d %H:%M:%S')
