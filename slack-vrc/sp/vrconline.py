aa3529412a4ba0af13b30a5e4fa49c3459e50ec7
Hiiragi Kugiha <46911299+kugiha@users.noreply.github.com>
2019-03-08 23:32:24 +0900

    Fetch instance and world from DB

--- slack-vrc/sp/vrconline.py
+++ slack-vrc/sp/vrconline.py
@@ -9,45 +9,55 @@ def online():
     reply = '\nYour online friends (as of {}):'.format(as_of_string)
     for f in friends:
         reply += '\n* '
-        if f.get('instance_type') == 'private':
+        world = getWorldInfoById(f['world_id'])
+        instance = getInstanceInfoById(f['world_id'], f['instance_id'])
+        if world['type'] == 'private':
             # private
             reply += '*{}* :lock:'.format(f['name'])
         else:
             # public, friends+, etc...
             reply += '*{}* _in {}_{}'.format(
-                f['name'], f['world_name'],
-                get_instanceStatus_emoji(f['instance_type']))
+                f['name'], world['name'],
+                get_instanceStatus_emoji(instance['type']))
     return reply
 
 
 def online_grouped():
     friends = getOnlineFriends()
     as_of_string = get_as_of_string(friends[0]['updated_at'])
-    users_by_world = {}
+    users_by_instance = {}
     private_users = []
     for f in friends:
-        if f.get('instance_type') == 'private':
+        world = getWorldInfoById(f['world_id'])
+        instance = getInstanceInfoById(f['world_id'], f['instance_id'])
+        if instance['instance_type'] == 'private':
             # private
             private_users.append(f)
         else:
             # public, friends+, etc...
-            if f['instance_id'] not in users_by_world.keys():
-                users_by_world[f['instance_id']] = []
-            users_by_world[f['instance_id']].append(f)
-    users_by_world = sorted(
-        users_by_world.items(),
+            if instance[
+                    'instance_id_concatenated_with_world_id'] not in users_by_instance.keys(
+                    ):
+                users_by_instance[
+                    instance['instance_id_concatenated_with_world_id']] = []
+            users_by_instance[
+                instance['instance_id_concatenated_with_world_id']].append(f)
+    users_by_instance = sorted(
+        users_by_instance.items(),
         key=lambda x: -len(x[1]))  # {'instance_id': [users array]}
     reply = '\nOnline friends (grouped) (as of {})\n'.format(as_of_string)
-    for item in users_by_world:
+    for (instance_id_concatenated_with_world_id, users) in users_by_instance:
+        world = getWorldInfoById(
+            instance_id_concatenated_with_world_id.split('/')[0])
+        instance = getInstanceInfoById(
+            *instance_id_concatenated_with_world_id.split('/'))
         reply += '* '
-        users = item[1]
         for user in users:
             reply += '*{}*, '.format(user['name'])
-        reply = reply[:-2]
+        reply = reply[:-2]  # Remove last ', '
         reply += ' _in {}_ ({}/{}) {}\n'.format(
-            users[0]['world_name'], users[0]['instance_users_count'],
-            users[0]['world_capacity'],
-            get_instanceStatus_emoji(users[0]['instance_type']))
+            world['name'], instance['users_count'], world['capacity'],
+            get_instanceStatus_emoji(instance['type']))
     reply += 'in-private friends\n'
     for user in private_users:
         reply += '{},'.format(user['name'])
